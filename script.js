99a45d7e993cf18526e35a25626c8acdef3e2ba5
Olek <example@gmail.com>
2021-07-18 21:11:56 +0200

    adding some scripts

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов ты посмотрел?", "");
+const personalMovieDatabase = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", ""),
+    c = prompt("Один из последних просмотренных фильмов?", ""),
+    d = prompt("На сколько оцените его?", "");
+
+personalMovieDatabase.movies[a] = b;
+personalMovieDatabase.movies[c] = d;
+
+console.log(personalMovieDatabase);
